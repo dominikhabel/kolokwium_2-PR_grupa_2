7582c4808ee70a3483d44de0c33fd87216f82059
dominikhab <dominik.habel7@gmail.com>
2021-06-11 12:05:36 +0200

    Kolokwium

--- src/components/posts.jsx
+++ src/components/posts.jsx
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-
+import Dhnavbar from "./dhnavbar";
 class Posts extends Component {
 
     constructor(props) {
@@ -39,12 +39,12 @@ class Posts extends Component {
             <React.Fragment>
                 {items.map((item, key) => {
                     return <div key={key}>
+                        <Dhnavbar/>
                         <div className="card">
-                            <img className="card-img-top" src="..." alt="Card image cap" />
+                            <img className="card-img-top" src="{item.image}" alt="Card image cap" />
                                 <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">Some quick example text to build on the card title and make
-                                        up the bulk of the card's content.</p>
+                                    <h5 className={item.title}>{item.title}</h5>
+                                    <p className="card-text">{item.text}</p>
                                     <a href="#" className="btn btn-primary">Go somewhere</a>
                                 </div>
                         </div>
